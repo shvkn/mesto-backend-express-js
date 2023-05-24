a1239fc38be8494ff11e6efc8ab53754ebd7c075
Anatoly Shuvakhin (shvkn) <a.shuvakhin@gmail.com>
2023-05-24 20:03:05 +0700

    feat(users) Реализовать роут GET /users/:userId - возвращает пользователя по _id

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 
 import User from '../models/user';
+import NotFoundError from '../shared/not-found-error';
+import { ErrorMessages } from '../shared/constants';
 
 // eslint-disable-next-line import/prefer-default-export
 export const getUsers = async (
@@ -11,6 +13,24 @@ export const getUsers = async (
   try {
     const users = await User.find({});
     res.send(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { userId } = req.params;
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      next(new NotFoundError(ErrorMessages.USER_NOT_FOUND));
+    } else {
+      res.send(user);
+    }
   } catch (error) {
     next(error);
   }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
-import { getUsers } from '../controllers/users';
+import { getUserById, getUsers } from '../controllers/users';
 
 const router = Router();
 
 router.get('/', getUsers);
+router.get('/:userId', getUserById);
 
 export default router;
