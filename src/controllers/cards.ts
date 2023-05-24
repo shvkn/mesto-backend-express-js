b90d75a1a41fdb5b7ac008d7102b45b99efd0cca
Anatoly Shuvakhin (shvkn) <a.shuvakhin@gmail.com>
2023-05-24 21:03:28 +0700

    feat(cards) Реализовать роут DELETE /cards/:cardId — удаляет карточку по идентификатору

--- src/controllers/cards.ts
+++ src/controllers/cards.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 
 import Card from '../models/card';
+import NotFoundError from '../shared/not-found-error';
+import { ErrorMessages } from '../shared/constants';
 
 export const getCards = async (
   req: Request,
@@ -25,6 +27,24 @@ export const createCard = async (
   try {
     const card = await Card.create({ name, owner, link });
     res.send(card);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteCard = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { cardId } = req.params;
+  try {
+    const card = await Card.findByIdAndDelete(cardId);
+    if (!card) {
+      next(new NotFoundError(ErrorMessages.CARD_NOT_FOUND));
+    } else {
+      res.send(card);
+    }
   } catch (error) {
     next(error);
   }
--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
-import { createCard, getCards } from '../controllers/cards';
+import { createCard, deleteCard, getCards } from '../controllers/cards';
 
 const router = Router();
 
 router.get('/', getCards);
 router.post('/', createCard);
+router.delete('/:cardId', deleteCard);
+
 export default router;
