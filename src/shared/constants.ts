5d3012d6c199ecb5a0ad64011574cde85aec3d4b
Anatoly Shuvakhin (shvkn) <a.shuvakhin@gmail.com>
2023-05-25 08:46:29 +0700

    fix(globalErrorHandler) Исправить сообщение ошибки по умолчанию

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,8 +1,11 @@
 export const ErrorNames = {
+  UNKNOWN_ERR: 'UnknownError',
   NOT_FOUND_ERR: 'NotFoundError',
   VALIDATION_ERR: 'ValidationError',
+  CAST_ERROR: 'CastError',
 };
 export const ErrorMessages = {
   CARD_NOT_FOUND: 'Запрашиваемая карточка не найдена',
   USER_NOT_FOUND: 'Запрашиваемый пользователь не найден',
+  DEFAULT: 'На сервере произошла ошибка',
 };
--- src/shared/global-error-handler.ts
+++ src/shared/global-error-handler.ts
@@ -1,24 +1,27 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
-import { ErrorNames } from './constants';
+import { ErrorMessages, ErrorNames } from './constants';
 
 const globalErrorHandler = (
   err: Error,
   req: Request,
   res: Response,
-  // eslint-disable-next-line no-unused-vars
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
   next: NextFunction,
 ) => {
-  const { message, name } = err;
+  const { name = ErrorNames.UNKNOWN_ERR } = err;
+  let { message = ErrorMessages.DEFAULT } = err;
 
   if (name === ErrorNames.NOT_FOUND_ERR) {
     res.status(StatusCodes.NOT_FOUND);
-  } else if (name === ErrorNames.VALIDATION_ERR) {
+  } else if (name === ErrorNames.VALIDATION_ERR || name === ErrorNames.CAST_ERROR) {
     res.status(StatusCodes.BAD_REQUEST);
   } else {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+    message = ErrorMessages.DEFAULT;
   }
+
   res.send({ message });
 };
 
