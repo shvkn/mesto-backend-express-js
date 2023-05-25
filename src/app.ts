caba47f5473a51ebdc4fd7a99675eda3abdd7a24
Anatoly Shuvakhin (shvkn) <a.shuvakhin@gmail.com>
2023-05-25 12:57:16 +0700

    feat(app) Добавить обработку несуществующих роутов

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import mongoose from 'mongoose';
 
 import userRoutes from './routes/users';
@@ -7,16 +7,22 @@ import cardRoutes from './routes/cards';
 
 import errorMiddleware from './middlewares/error-middleware';
 import authMiddleware from './middlewares/auth-middleware';
+import NotFoundError from './shared/not-found-error';
+import { ErrorMessages } from './shared/constants';
 
 dotenv.config();
-const { PORT = 3000, DB_URL = '' } = process.env;
+const {
+  PORT = 3000,
+  DB_URL = ''
+} = process.env;
 
 const app = express();
 
-mongoose.connect(DB_URL).catch((error) => {
-  // eslint-disable-next-line no-console
-  console.log(error);
-});
+mongoose.connect(DB_URL)
+  .catch((error) => {
+    // eslint-disable-next-line no-console
+    console.log(error);
+  });
 
 app.use(express.json());
 app.use(authMiddleware);
@@ -24,9 +30,13 @@ app.use(authMiddleware);
 app.use('/users', userRoutes);
 app.use('/cards', cardRoutes);
 
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  next(new NotFoundError(ErrorMessages.WRONG_ROUTE));
+});
+
 app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server has been starter at port: ${PORT}`);
+  console.log(`Server has been started at port: ${PORT}`);
 });
